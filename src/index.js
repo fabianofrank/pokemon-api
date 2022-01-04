ec923d3ba5b0799a65ecc171496524a5ee118076
fabianofrank <fabianofrank@protonmail.com>
2022-01-04 18:13:32 -0300

    Add modal-popup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+import { pokeCall } from './comments.js';
+
+let pokeArr;
+const getPokemon = () => {
+  pokeCall().then((Allpoke) => {
+    pokeArr = Allpoke.results;
+    console.log(pokeArr);
+  });
+};
+
+getPokemon();
