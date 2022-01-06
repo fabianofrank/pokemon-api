bede4e77e4282773c67d7637983a3329075daf8e
fabianofrank <fabianofrank@protonmail.com>
2022-01-06 12:14:18 -0300

    Clean comment.js into html template

--- src/comments.js
+++ src/comments.js
@@ -1,7 +1,8 @@
-/* eslint-disbale import/no-cycle */
+/* eslint-disbale import/no-cycle no-unused-vars no-undef */
 import { pokeCall } from './apiCall.js';
 import close from './cancel.png';
 
+// Involment POST and GET
 const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/ntSEDKBSp5jVB8zr1TJB/comments';
 
 const postCom = async (e) => {
@@ -20,103 +21,60 @@ async function getCom() {
   return response.json();
 }
 
+// Display Stats
 const commentWindow = document.getElementById('comment-window');
+
+const toggleClose = () => {
+  if (commentWindow.style.display === 'block') {
+    commentWindow.style.display = 'none';
+    commentWindow.innerHTML = '';
+  }
+};
 
 const displayPokemonStats = (e) => {
   pokeCall(e).then((poke) => {
     const pokeImg = poke.sprites.front_shiny;
     const pokeTypeOne = poke.types[0].type.name;
-    const pokeName = poke.name;
+    const pokeName = poke.name.toUpperCase();
     const pokeWeight = poke.weight;
     const pokeHeight = poke.height;
     const pokePower = poke.base_experience;
 
-    // Stats Section
+    let html = '';
+    const htmlSegment = `
+    <div id="stats-window">
+      <div id="stats-section">
+        <img src="${close}" id="close" width="48">
+        <img src="${pokeImg}" id="photo" width="160">
+      </div>
+      <h2>${pokeName}</h2>
+      <div id="type-div">
+        <p id="type-tex">${pokeTypeOne}</p>
+      </div>
+      <p>Weight: ${pokeWeight}</p>
+      <p>Height: ${pokeHeight}</p>
+      <p>Power: ${pokePower}</p>
+      <comments-section id="comment-section" class="comments-section">
+        <h3>Best Comments</h3>
+        <div class="container"></div>
+        <ul class="comments-list" style="display: none;"></ul>
+        <h3>Add your comment</h3>
+        <form class="form-class">
+          <input class="input-name" placeholder="Your name" required="">
+          <textarea class="input-text" placeholder="Your comment" required=""></textarea>
+          <button type="submit" class="send-comment">Send your comment</button>
+        </form>
+      </comments-section>
+    </div>
+    `;
+    html += htmlSegment;
+    commentWindow.innerHTML = html;
 
-    const statsWindow = document.createElement('div');
-    statsWindow.id = 'stats-window';
-    const headSection = document.createElement('div');
-    headSection.id = 'stats-section'
-    const cancelImage = document.createElement('img');
-    cancelImage.width = '48';
-    cancelImage.src = close;
-    cancelImage.id = 'close';
-    const img = document.createElement('img');
-    img.src = pokeImg;
-    img.width = '160';
-    img.id = 'photo';
-    const name = document.createElement('h2');
-    name.innerText = pokeName[0].toUpperCase() + pokeName.slice(1);
-    const typeDiv = document.createElement('div');
-    typeDiv.id = 'type-div';
-    const type = document.createElement('p');
-    type.id = 'type-tex'
-    type.innerText = `${pokeTypeOne[0].toUpperCase() + pokeTypeOne.slice(1)}`;
-    const weight = document.createElement('p');
-    weight.innerText = `Weight: ${pokeWeight} lbs`;
-    const height = document.createElement('p');
-    height.innerText = `Height: ${pokeHeight} ft`;
-    const power = document.createElement('p');
-    power.innerText = `Power: ${pokePower} pts`;
-    
-    // Comment Section
-    
-    const commentSection = document.createElement('comments-section');
-    commentSection.id = 'comment-section';
-    commentSection.classList.add('comments-section');
-    const titleComment = document.createElement('h3');
-    titleComment.textContent = "Best Comments";
-    const listComments = document.createElement('ul');
-    listComments.classList.add('comments-list');
-    listComments.style.display = 'none';
-    const leaveComment = document.createElement('h3');
-    leaveComment.textContent = "Add your comment";
-    const formComments = document.createElement('form');
-    formComments.classList.add('form-class');
-    const nameComment = document.createElement('input');
-    nameComment.classList.add('input-name');
-    nameComment.placeholder = "Your name";
-    nameComment.setAttribute("required", "");
-    nameComment.required = true;   
-    const textComment = document.createElement('textarea');
-    textComment.classList.add('input-text');
-    textComment.placeholder = "Your comment";
-    textComment.setAttribute("required", "");
-    textComment.required = true; 
-    const btnSubmitComment = document.createElement('button');
-    btnSubmitComment.setAttribute("type", "submit");
-    btnSubmitComment.classList.add('send-comment');
-    btnSubmitComment.textContent = "Send your comment";
-
-    commentSection.appendChild(titleComment);
-    commentSection.appendChild(listComments);
-    commentSection.appendChild(leaveComment);
-    commentSection.appendChild(formComments);
-    formComments.appendChild(nameComment);
-    formComments.appendChild(textComment);
-    formComments.appendChild(btnSubmitComment);
-    commentWindow.appendChild(statsWindow);
-    
-    headSection.appendChild(cancelImage);
-    headSection.appendChild(img);
-    statsWindow.appendChild(headSection);
-    statsWindow.appendChild(name);
-    typeDiv.appendChild(type);
-    statsWindow.appendChild(typeDiv);
-    statsWindow.appendChild(weight);
-    statsWindow.appendChild(height);
-    statsWindow.appendChild(power);
-    statsWindow.appendChild(commentSection);
-    
-    cancelImage.addEventListener('click', () => {
-      if (commentWindow.style.display === 'block') {
-        commentWindow.style.display = 'none';
-        commentWindow.innerHTML = '';
-      }
-    });
+    document.getElementById('close').addEventListener('click', toggleClose);
   });
 };
 
+// Toggle: open/close each card
 const toggle = (id) => {
   if (commentWindow.style.display === 'none') {
     commentWindow.style.display = 'block';
