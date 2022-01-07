e70ee303b6d067c719a66a033771cac2b26250e3
Oliver Coimbra <olivercoimbra@gmail.com>
2022-01-07 19:51:24 -0400

    Fix linter errors

--- src/__test__/adoptComment.test.js
+++ src/__test__/adoptComment.test.js
@@ -3,12 +3,11 @@
  */
 const serverData = [{item: '1'}, {item:'2'}];
 
-
 const counter = (serverData) => {
   return document.createElement('div').innerHTML = `(${serverData.length})`;
 };
  
  test('Counts serverData in div tag and sets value as innerHTML', () => {
- 
   expect(counter(serverData)).toBe('(2)');
  });
+ 
