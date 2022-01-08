78d4b00c3ff9c6c8ddab4c5d4e19f1984172b299
Oliver Coimbra <olivercoimbra@gmail.com>
2022-01-07 20:17:42 -0400

    Fix linter errors

--- src/__test__/adoptComment.test.js
+++ src/__test__/adoptComment.test.js
@@ -1,8 +1,8 @@
+/* eslint-disable arrow-body-style no-return-assign */
 /**
  * @jest-environment jsdom
  */
 const serverData = [{ item: '1' }, { item: '2' }];
-/* eslint-disable arrow-body-style no-return-assign */
 const counter = (serverData) => { return document.createElement('div').innerHTML = `(${serverData.length})`; };
 test('Counts serverData in div tag and sets value as innerHTML', () => {
   expect(counter(serverData)).toBe('(2)');
