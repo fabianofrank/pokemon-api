9f7ac7fa7916a968f024e1000f06f741ace58575
fabianofrank <fabianofrank@protonmail.com>
2022-01-07 21:29:48 -0300

    Linters fixed

--- src/__test__/adoptComment.test.js
+++ src/__test__/adoptComment.test.js
@@ -1,9 +1,10 @@
-/* eslint-disable arrow-body-style no-return-assign */
+/* eslint-disbale arrow-body-style */
+/* eslint-disable no-return-assign */
 /**
  * @jest-environment jsdom
  */
 const serverData = [{ item: '1' }, { item: '2' }];
-const counter = (serverData) => { return document.createElement('div').innerHTML = `(${serverData.length})`; };
+const counter = (serverData) => document.createElement('div').innerHTML = `(${serverData.length})`;
 test('Counts serverData in div tag and sets value as innerHTML', () => {
   expect(counter(serverData)).toBe('(2)');
 });
